e99c51d8ca5b514924c578aedef761eced690c4b
Stephanie Tahova <106015292+sstephanie7@users.noreply.github.com>
2022-06-11 14:07:24 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace TopInteger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] inputArray = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                bool currIterationBiggestNum = true; 
+                for (int k = i + 1; k < inputArray.Length; k++)
+                {
+                    if (inputArray[i] <= inputArray[k])
+                    {
+                        currIterationBiggestNum = false;
+                    }
+                }
+                if (currIterationBiggestNum)
+                {
+                    Console.Write($"{inputArray[i]} ");
+                }
+            }
+        }
+    }
+}
